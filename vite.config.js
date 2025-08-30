0e520f3d2a15af8d40f64e010e96206aca2855b5
willgee9531 <willgee9531@gmail.com>
2025-08-30 01:13:50 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://willgee9531.github.io/json-server/jobs.json',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
-  base: '/',
+  base: '/react-jobs/',
 });
 
 // Edit this file to use github pages
